8eb307c0f926a1a84ecb59d3ca97dabd644a38b1
Guilherme <guilhermegheno@gmail.com>
2026-01-12 20:52:21 -0300

    Hero nao sai mais

--- script.js
+++ script.js
@@ -17,25 +17,6 @@ document
     });
   });
 
-/* HERO*/
-const hero = document.getElementById("hero");
-const heroText = document.querySelector(".hero-text");
-
-window.addEventListener("scroll", () => {
-  if (!hero || !heroText) return;
-
-  const heroBottom = hero.offsetTop + hero.offsetHeight;
-  const scrollPos = window.scrollY + window.innerHeight / 2;
-
-  const isMobile = window.innerWidth <= 768;
-  const offset = isMobile ? -200 : 50; // mobile some antes
-
-  heroText.style.opacity =
-    scrollPos > heroBottom - offset ? "0" : "1";
-
-  heroText.style.pointerEvents =
-    scrollPos > heroBottom - offset ? "none" : "auto";
-});
 
 /* SCROLL SUAVE */
 document.querySelectorAll('a[href="#contato"]').forEach((link) => {
